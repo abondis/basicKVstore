8d98046aca09b37a7b95a3c508b9df29c71d66c7
Aurelien Bondis <abondis@pheromone.ca>
2015-09-04 20:20:47 -0400

    basic kv store

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='BasicKVStore',
+      version='0.1',
+      description='Basic KV store',
+      author='A. Bondis',
+      author_email='abondis@kerunix.com',
+      packages=[''],
+)
